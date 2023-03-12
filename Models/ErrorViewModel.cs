c00bbca074859714d209cfedae57f13099d611fb
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-03-12 01:29:20 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Songify_FullStack.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
